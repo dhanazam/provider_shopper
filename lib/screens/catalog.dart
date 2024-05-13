5e91128810ed292441c5fd645ca585acb285e13f
Ramadhan <ramadanazam93@gmail.com>
2024-05-13 17:23:24 +0800

    add plain screen

---
+++ lib/screens/catalog.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyCatalog extends StatelessWidget {
+  const MyCatalog({ super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Catalog Page')
+      )
+    );
+  }
+}
