34f20daf5314977fb0c74a5f36aa2ddb3b4667b6
Ramadhan <ramadanazam93@gmail.com>
2024-05-13 15:30:38 +0800

    add login screen

---
+++ lib/screens/login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({ super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Login Page')
+      )
+    );
+  }
+}
