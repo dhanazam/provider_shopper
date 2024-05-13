572d2d3aaaad0e61333c9ab404b3e5e9e2fb7c6e
Ramadhan <ramadanazam93@gmail.com>
2024-05-13 23:23:57 +0800

    rename the title

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Provider Demo',
+      title: 'Shopping',
       routerConfig: router(),
     );
   }
