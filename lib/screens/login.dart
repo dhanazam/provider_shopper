531fd8f412b74b70e36ebef35343943508ffc397
Ramadhan <ramadanazam93@gmail.com>
2024-05-14 12:28:24 +0800

    add login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,9 +5,43 @@ class MyLogin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Login Page')
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Username'
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // context.pushReplacement('/catalog');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                child: const Text('ENTER'),
+              )
+            ]
+          )
+        )
       )
     );
   }
