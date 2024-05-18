8fa4034785f81ba37da3df361a05c1ef3d59154b
Ramadhan <ramadanazam93@gmail.com>
2024-05-19 00:59:04 +0800

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,63 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider_shopper/models/cart.dart';
+import 'package:provider_shopper/models/catalog.dart';
 import 'package:provider_shopper/common/theme.dart';
 import 'package:provider_shopper/screens/login.dart';
 import 'package:provider_shopper/screens/catalog.dart';
 import 'package:provider_shopper/screens/cart.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
 GoRouter router() {
-  return GoRouter(
-    initialLocation: '/login',
-    routes: [
-      GoRoute(
-        path: '/login',
-        builder: (context, state) => const MyLogin()
-      ),
-      GoRoute(
-        path: '/catalog',
-        builder: (context, state) => const MyCatalog(),
-        routes: [
-          GoRoute(
-            path: 'cart',
-            builder: (context, state) => const MyCart(),
-          ),
-        ],
-      )
-    ]
-  );
+  return GoRouter(initialLocation: '/login', routes: [
+    GoRoute(path: '/login', builder: (context, state) => const MyCatalog()),
+    // GoRoute(
+    //   path: '/catalog',
+    //   builder: (context, state) => const MyCatalog(),
+    //   routes: [
+    //     GoRoute(
+    //       path: 'cart',
+    //       builder: (context, state) => const MyCart(),
+    //     ),
+    //   ],
+    // )
+  ]);
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Shopping',
-      theme: appTheme,
-      routerConfig: router(),
+    // Using MultiProvider is convenient when providing multiple objects.
+    return MultiProvider(
+      providers: [
+        // In this sample app, CatalogModel never changes, so a simple Provider
+        // is sufficient.
+        Provider(create: (context) => CatalogModel()),
+        // CartModel is implemented as a ChangeNotifier, which calls for the use
+        // of ChangeNotifierProvider. Moreover, CartModel depends
+        // on CatalogModel, so a ProxyProvider is needed.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Provider Demo',
+        theme: appTheme,
+        routerConfig: router(),
+      ),
     );
   }
 }
